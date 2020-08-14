b1575f8427602ebe13118f0cc22b3e96e7cc6ea1
xianergoo <xianergoo@gmail.com>
2020-08-15 00:54:11 +0800

    code improve

--- dev/main.py
+++ dev/main.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello Flask!'
+def index():
+    return render_template('index.html')
+
+@app.route('/home/users/<string:name>/posts/<int:id>')
+def hello(name, id):
+    return "Hello, " +  name + ", your id is: " + str(id)
+
+@app.route('/onlyget', methods=['GET'])
+def get_req():
+    return "You can only get this weboage."
+
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
