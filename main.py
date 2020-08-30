12d2815b01d14affff35d93afc48ceb8f7a6f8ed
xianergoo <xianergoo@gmail.com>
2020-08-30 19:37:28 +0800

    add file

--- main.py
+++ main.py
@@ -3,11 +3,24 @@ from flask import render_template
 
 app = Flask(__name__)
 
-
+all_posts = [
+    {
+        'title': 'Post 1',
+        'author': 'alex',
+        'content': 'This is the content of post 1. bulabula'
+    },
+    {
+        'title': 'Post 2',
+        'content': 'This is the content of post 2. bulabula'
+    }
+]
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', posts=all_posts)
 
 @app.route('/home/users/<string:name>/posts/<int:id>')
 def hello(name, id):
