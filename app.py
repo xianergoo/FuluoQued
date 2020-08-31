5e7913a69744c9c73a7a832c9f85f1b5fa98c3c2
xianergoo <xianergoo@gmail.com>
2020-08-31 20:42:27 +0800

    add SQLALchemy Feature

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db=SQLAlchemy(app)
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(20), nullable=False, default='N/A')
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Bolg Post' + str(self.id)
 
 all_posts = [
     {
@@ -14,6 +28,7 @@ all_posts = [
         'content': 'This is the content of post 2. bulabula'
     }
 ]
+
 @app.route('/')
 def index():
     return render_template('index.html')
